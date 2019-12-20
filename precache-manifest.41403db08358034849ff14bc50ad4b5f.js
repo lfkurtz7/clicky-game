8041695c2d4f160db35462725593146ff406c254
Leah Kurtz <lfkurtz7@gmail.com>
2019-12-20 10:06:43 -0500

    Updates

---
+++ precache-manifest.41403db08358034849ff14bc50ad4b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9964e642e177d6800a813f65d8e31f0c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a919e59023daf3b3d11a",
+    "url": "/clicky-game/static/css/main.6d094e64.chunk.css"
+  },
+  {
+    "revision": "e936e937484ed10beb5d",
+    "url": "/clicky-game/static/js/2.ca6d020b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.ca6d020b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a919e59023daf3b3d11a",
+    "url": "/clicky-game/static/js/main.98032a89.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
